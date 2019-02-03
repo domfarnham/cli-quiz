cb6e997006d8859977a4861293ff1ce2e19940af
Dom Farnham <dominic.farnham@hotmail.co.uk>
2019-02-03 16:06:14 +0000

    Create quiz.py

---
+++ src/quiz.py
@@ -0,0 +1,22 @@
+# built-in
+import datetime
+import random
+# my-own
+from questions import Add, Multiply
+
+
+class Quiz:
+  questions = []
+  answers = []
+  
+  def __init__(self):
+    pass
+    
+  def take_quiz(self):
+    pass
+    
+  def ask(self, question):
+    pass
+  
+  def summary(self):
+    pass
