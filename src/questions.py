ae1ac184cd7912f07958784f2d1a369e79f04795
domfarnham <dominic.farnham@hotmail.co.uk>
2019-02-11 19:58:14 +0000

    Fixed indentation issues

--- src/questions.py
+++ src/questions.py
@@ -1,14 +1,14 @@
 class Question:
-  answer = None
-  text = None
-  
+    answer = None
+    text = None
+
 
 class Add(Question):
     def __init__(self, num1, num2):
         self.text = f'{num1} + {num2}'
         self.answer = num1 + num2
 
-    
+
 class Multiply(Question):
     def __init__(self, num1, num2):
         self.text = f'{num1} x {num2}'
